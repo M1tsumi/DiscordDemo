d7600ce3348ed7d64d83c1ac01adc234852afadc
Thomas Cobb <88093506+M1tsumi@users.noreply.github.com>
2025-07-29 15:58:48 -0700

    Requirements Installer Development Push - Not Required
    This is optional if you know what you're doing thoroughly. If you do not know what your doing this is a good option, but make sure that you are careful as mistakes are always possible!

---
+++ start-bot-manager.bat
@@ -0,0 +1,21 @@
+@echo off
+title Discord Bot Manager
+color 0A
+
+echo.
+echo ===========================================
+echo    DISCORD BOT MANAGER LAUNCHER
+echo ===========================================
+echo.
+
+echo Starting Bot Manager...
+echo This will open your browser automatically.
+echo.
+echo If the browser doesn't open, go to: http://localhost:3000
+echo.
+echo Press Ctrl+C to stop the bot manager.
+echo.
+
+node launcher.js
+
+pause 
